efa8148da2be6fe14ede30cb7e87ee26524b64c2
Marat Kasimov <kasimov.mv@mail.ru>
2020-05-15 00:38:30 +0400

    add AfterEach

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,7 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class First {
 
@@ -12,11 +14,19 @@ public class First {
     @BeforeEach
      public void start (){
         WebDriverManager.operadriver().setup();
-        System.out.println("before each test");
+        driver = new OperaDriver();
+
+//        System.out.println("before each test");
+    }
+
+    @AfterEach
+    public void finish(){
+        driver.quit();
     }
 
     @Test
     public void firstTest (){
+        driver.get(SITE_URL);
 
     }
 
