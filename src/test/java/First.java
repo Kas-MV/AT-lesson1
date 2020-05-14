dc375dde1d5adbc9eb7eea70bd589a243b4d6e23
Marat Kasimov <kasimov.mv@mail.ru>
2020-05-15 00:24:28 +0400

    Initial commit

---
+++ src/test/java/First.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class First {
+
+    String SITE_URL = "https://mvnrepository.com/";
+
+    WebDriver driver;
+
+    @Test
+    public void firstTest (){
+
+    }
+
+    @Test
+    public void secondTest (){
+
+    }
+}
