d93cda0badd543e043126057141ad58471778074
Marat Kasimov <kasimov.mv@mail.ru>
2020-05-15 00:31:56 +0400

    add WebDriverManager.operadriver

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -1,3 +1,5 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
@@ -6,6 +8,12 @@ public class First {
     String SITE_URL = "https://mvnrepository.com/";
 
     WebDriver driver;
+
+    @BeforeEach
+     public void start (){
+        WebDriverManager.operadriver().setup();
+        System.out.println("before each test");
+    }
 
     @Test
     public void firstTest (){
